f43688da345d16d741cdc351c6143a4fb583746f
LuisaFC <luisabsb@gmail.com>
2022-06-17 12:44:20 -0300

    Finish

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const users = require('../mocks/users');
+let users = require('../mocks/users');
 
 module.exports = {
   listUsers(request, response){
@@ -38,6 +38,40 @@ module.exports = {
 
       response.send(200, newUser)
     
+  },
+
+  updateUser(request, response){
+    let { id } = request.params;
+    const { name } = request.body
+
+    id = Number(id);
+
+    const userExists = users.find((user) => user.id === id);
+
+    if(!userExists){
+      return response.send(400, { error: 'User not found'})
+    }
+
+    users = users.map((user) => {
+      if(user.id === id ){
+        return {
+          ...user,
+          name,
+        }
+      }
+
+      return user
+    })
+
+    response.send(200, { id, name})
+  },
+
+  deleteUser(request, response){
+    let { id } = request.params;
+    id = Number(id);
+
+    users = users.filter((user) => user.id !== id)
+    response.send(200, { deletes: true})
   }
 }
  
